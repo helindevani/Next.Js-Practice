166ac0497c03bd033743299be2d9c8078d7f06c6
helindevani <helindevani111@gmail.com>
2024-02-22 22:39:16 +0530

    completed till data prefetching

---
+++ Data_Fetching_05/pages/user-profile.js
@@ -0,0 +1,17 @@
+export default function UserProfile(props){
+    return (
+        <h1>{props.username}</h1>
+    )
+}
+
+export async function getServerSideProps(context){
+
+    const { params, req, res} = context;
+
+    return{
+        props : {
+            username : 'Max',
+        }
+    }
+
+}
