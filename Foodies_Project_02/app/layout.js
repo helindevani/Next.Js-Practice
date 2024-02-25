4c3065f90c08c0532ef7bd5a4ac7e6d162113326
helindevani <helindevani111@gmail.com>
2024-02-25 16:04:15 +0530

    complete till project with app router

--- Foodies_Project_02/app/layout.js
+++ Foodies_Project_02/app/layout.js
@@ -10,7 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        
+        // that is layout page
         <MainHeader />
         {children}
       </body>
